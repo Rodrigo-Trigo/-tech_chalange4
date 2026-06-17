331d89a8b1a59ef7e42cbd18279f3dcfa2c52316
Rodrigo-Trigo <rodrigo_jtj94@hotmail.com>
2026-06-17 19:20:23 -0300

    Update main.tf

--- FIAP/terraform/eks/main.tf
+++ FIAP/terraform/eks/main.tf
@@ -1,1 +1,13 @@
-'resource "aws_eks_cluster" "main" { name = "fiap-eks" role_arn = "arn:aws:iam::123456789012:role/EKSRole" }' 
+# EKS Cluster Configuration
+resource "aws_eks_cluster" "main" {
+  name     = "fiap-eks"
+  role_arn = "arn:aws:iam::123456789012:role/EKSRole"
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public.id]
+  }
+
+  tags = {
+    Environment = "fase4"
+  }
+}

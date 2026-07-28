c3b36563f718b4402647cc2d0eef7ed41721d0c8
Rodrigo-Trigo <rodrigo_jtj94@hotmail.com>
2026-07-28 19:09:36 -0300

    Update main.tf

--- FIAP/terraform/eks/main.tf
+++ FIAP/terraform/eks/main.tf
@@ -1,13 +1,27 @@
-# EKS Cluster Configuration
-resource "aws_eks_cluster" "main" {
+resource "aws_eks_cluster" "fiap" {
   name     = "fiap-eks"
-  role_arn = "arn:aws:iam::123456789012:role/EKSRole"
+  role_arn = "arn:aws:iam::656894455947:role/LabRole"
 
   vpc_config {
-    subnet_ids = [aws_subnet.public.id]
+    subnet_ids = [
+      aws_subnet.public_a.id,
+      aws_subnet.public_b.id
+    ]
   }
+}
 
-  tags = {
-    Environment = "fase4"
+resource "aws_eks_node_group" "fiap_nodes" {
+  cluster_name    = aws_eks_cluster.fiap.name
+  node_group_name = "fiap-nodes"
+  node_role_arn   = "arn:aws:iam::656894455947:role/LabRole"
+  subnet_ids      = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 }
